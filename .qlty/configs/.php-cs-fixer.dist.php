3253594f598920dbc33eeebd8bcc1ae02d0f4e21
Ben Carey <bdmc@sinemacula.co.uk>
2026-04-16 16:36:13 -0400

    chore(qlty): update php-cs-fixer configuration

--- .qlty/configs/.php-cs-fixer.dist.php
+++ .qlty/configs/.php-cs-fixer.dist.php
@@ -8,4 +8,6 @@ return PhpCsFixerConfig::make([
     dirname(__DIR__, 2) . '/benchmarks',
     dirname(__DIR__, 2) . '/database',
     dirname(__DIR__, 2) . '/tests',
+], [
+    'php_unit_test_class_requires_covers' => false,
 ]);
